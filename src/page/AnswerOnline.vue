04d46cf74ad0e96da366d0a7440774328efd7e96
Absent <1632846188@qq.com>
2020-10-22 11:49:56 +0800

    '答题页面静态'

--- src/page/AnswerOnline.vue
+++ src/page/AnswerOnline.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="background-color: antiquewhite;height: 100%">
     <div class="top">
       <div style="padding-top:1px;width: 70%;">
         <h1>在线笔试平台</h1>
@@ -11,9 +11,39 @@
       </div>
     </div>
     <div class="content">
-        <div style="width: 100%;height: 20%;background-color: black">timu</div>
-        <div style="width: 100%;height: 60%;background-color: yellow">neirong</div>
-        <div style="width: 100%;height: 20%;background-color: red">fanye</div>
+      <div style="width: 100%;height: 20%;">
+        <div style="width: 30%;float: left;"><h1>单选题</h1></div>
+      </div>
+      <div style="width: 100%;height: 60%;">
+        <div style="width: 100%;">
+          <div style="width: 60%;text-align: left;margin-left: 20%;font-size: 25px">
+            <ul v-for="(item,i) in examinationData" :key="i">
+              <div v-if="choose == i">
+                <div>{{i+1}}、{{item.question}}</div>
+                <li v-for="(son,index) in item.answer" :key="index">
+                  <div style="border: black;border-width: 1px;border-style: solid;margin-top: 5%;">
+                  <el-radio
+                    style="margin-left: 5%;margin-bottom: 2%"
+                    v-model="radio[i]"
+                    :label="son.value"
+                    :name="son.name"
+                    @change="getIputValue(i)"
+                  ></el-radio>
+                  </div>
+                </li>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div style="margin-left:20%;width: 60%;height: 20%;">
+        <div style="float: left;margin-left: 5%">
+          <el-button type="danger" :onclick="pre" style="position:relative">上一题</el-button>
+        </div>
+        <div style="float: right">
+          <el-button type="success" :onclick="next" style="position:relative">下一题</el-button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,10 +56,91 @@
         day: 0,
         hr: 0,
         min: 0,
-        sec: 0
+        sec: 0,
+        choose:'2',
+        question: "为初始化其成员变量，每个类都定义的方法是（）",
+        examinationData: [
+          {
+            id: "1",
+            name: "one",
+            question:
+              "为初始化其成员变量，每个类都定义的方法是（）",
+            answer: [
+              {value: "A.Python"},
+              {value: "B.Vue"},
+              {value: "C.Php"},
+              {value: "D.Java"}
+            ]
+          },
+          {
+            id: "1",
+            name: "two",
+            question:
+              "为初始化其成员变量，每个类都定义的方法是（）",
+            answer: [
+              {value: "A.问题一"},
+              {value: "B.Vue2"},
+              {value: "C.Php2"},
+              {value: "D.Java2"}
+            ]
+          },
+          {
+            id: "1",
+            name: "three",
+            question:
+              "为初始化其成员变量，每个类都定义的方法是（）",
+            answer: [
+              {value: "A.问题二"},
+              {value: "B.Vue3"},
+              {value: "C.Php3"},
+              {value: "D.Java3"}
+            ]
+          },
+          {
+            id: "1",
+            name: "four",
+            question:
+              "为初始化其成员变量，每个类都定义的方法是（）",
+            answer: [
+              {
+                value:
+                  "A.为初始化其成员变量，每个类都定义的方法是（）"
+              },
+              {value: "B.Vue4"},
+              {value: "C.Php4"},
+              {value: "D.Java4"}
+            ]
+          },
+          {
+            id: "1",
+            name: "five",
+            question: "How about your in skills?",
+            answer: [
+              {value: "A.问题四"},
+              {value: "B.Vue5"},
+              {value: "C.Php5"},
+              {value: "D.Java5"}
+            ]
+          }
+        ],
+        allRadio: [],//提交给后台的真题数据
+        radio: []//单选真题答案
       };
     },
     methods: {
+      pre(){
+        this.choose--;
+      },
+      next(){
+        this.choose++;
+      },
+      //答题
+      getIputValue(index) {
+        // console.log(index + 1 + "题" + this.radio);
+        this.allRadio[index] = this.radio[index]; // 将数据存入提交给后台的数据中
+        console.log(this.allRadio);
+        // console.log(this.radio)
+      },
       countdown() {
         const end = Date.parse(new Date('2099-05-06 00:00:00'));
         const now = Date.parse(new Date());
@@ -64,6 +175,7 @@
   html, body {
     padding: 0px;
     margin: 0px;
+    background-color: antiquewhite;
   }
 
   .jsq {
@@ -96,9 +208,16 @@
   .content {
     width: 60%;
     height: 600px;
-    background-color: yellow;
+    background-color: white;
+    border-color: aliceblue;
+    border-width: 3px;
+    border-style: solid;
     margin-left: 20%;
     margin-top: 2%;
     text-align: center;
+  }
+
+  ul, li {
+    list-style: none;
   }
 </style>
