aa663a8fe7ae0b8f49b48befb7eb1d86357ca1af
Absent <1632846188@qq.com>
2020-11-02 10:49:38 +0800

    ''

--- src/page/Enterprise/ReleaseTest.vue
+++ src/page/Enterprise/ReleaseTest.vue
@@ -14,7 +14,8 @@
       <div style="width: 60%;margin-left: 20%;height: 100%">
         <el-container style="height: 100%">
           <el-aside width="20%" style="background-color: rgb(238, 241, 246);height: 100%">
-            <el-menu default-active="1-4-1" style="height: 100%" class="el-menu-vertical-demo":default-active="this.$router.path"
+            <el-menu default-active="1-4-1" style="height: 100%" class="el-menu-vertical-demo"
+                     :default-active="this.$router.path"
                      router>
               <el-menu-item index="/Enterprise_question">
                 <i class="el-icon-menu"></i>
@@ -43,18 +44,24 @@
                 <el-step title="考试人员"></el-step>
               </el-steps>
               <div v-if="active == 1" style="width: 80%;margin-left: 10%;margin-top: 10%">
-                <el-form ref="form" :model="form" label-width="80px">
+                <el-form ref="examination" :model="examination" label-width="120px">
                   <el-form-item label="笔试名称">
-                    <el-input v-model="form.name" style="width: 45%;"></el-input>
+                    <el-input v-model="examination.name" style="width: 45%;"></el-input>
                   </el-form-item>
-                  <el-form-item label="笔试时间">
+                  <el-form-item label="笔试开始时间">
                     <el-col :span="11">
-                      <el-date-picker type="date" placeholder="选择日期" v-model="form.date1"
+                      <el-date-picker type="date" placeholder="选择日期" v-model="examination.beginTime"
+                                      style="width: 100%;"></el-date-picker>
+                    </el-col>
+                  </el-form-item>
+                  <el-form-item label="笔试结束时间">
+                    <el-col :span="11">
+                      <el-date-picker type="date" placeholder="选择日期" v-model="examination.endTime"
                                       style="width: 100%;"></el-date-picker>
                     </el-col>
                   </el-form-item>
                   <el-form-item label="是否启动" prop="form">
-                    <el-switch v-model="form.delivery"></el-switch>
+                    <el-switch v-model="examStatus"></el-switch>
                   </el-form-item>
                   <el-form-item>
                     <div style="text-align: center">
@@ -64,25 +71,24 @@
                 </el-form>
               </div>
               <div v-if="active == 2" style="width: 80%;margin-left: 10%;margin-top: 10%">
-                <el-form ref="form" :model="form2" label-width="80px">
+                <el-form ref="examination" :model="examination" label-width="120px">
                   <el-form-item label="笔试类型">
-                    <el-select v-model="form2.type" placeholder="请选择笔试类型">
-                      <el-option label="前端" value="shanghai"></el-option>
-                      <el-option label="后端" value="beijing"></el-option>
+                    <el-select v-model="examination.type" placeholder="请选择笔试类型">
+                      <el-option label="前端" value="前端"></el-option>
+                      <el-option label="后端" value="后端"></el-option>
                     </el-select>
                   </el-form-item>
-                  <el-form-item label="选择来源">
-                    <el-select v-model="form2.source" placeholder="请选择笔试类型">
-                      <el-option label="公共题库" value="shanghai"></el-option>
-                      <el-option label="企业题库" value="beijing"></el-option>
-                    </el-select>
-                  </el-form-item>
+                  <!--<el-form-item label="选择来源">-->
+                  <!--<el-select v-model="examination.source" placeholder="请选择笔试类型">-->
+                  <!--<el-option label="公共题库" value="公共题库"></el-option>-->
+                  <!--<el-option label="企业题库" value="企业题库"></el-option>-->
+                  <!--</el-select>-->
+                  <!--</el-form-item>-->
                   <el-form-item label="题目数量">
-                    <el-input-number v-model="form2.number" @change="handleChange" :min="1" :max="10"
-                                     label="描述文字"></el-input-number>
+                    <el-input-number v-model="examination.questionNumber" :min="1"></el-input-number>
                   </el-form-item>
                   <el-form-item label="考试时间">
-                    <el-time-picker arrow-control placeholder="选择时间" v-model="form2.time"
+                    <el-time-picker arrow-control placeholder="选择时间" v-model="examination.continueTime"
                                     style="width: 35%;"></el-time-picker>
                   </el-form-item>
 
@@ -98,12 +104,12 @@
                   :titles="['候选人', '考试人员']"
                   :filter-method="filterMethod"
                   filter-placeholder="请输入候选人 "
-                  v-model="value"
+                  v-model="chooseValue"
                   :data="data">
                 </el-transfer>
                 <div style="text-align: center">
                   <el-button style="margin-top: 12px;" type="primary" @click="pre">上一步</el-button>
-                  <el-button style="margin-top: 12px;" type="danger" @click="next">提交</el-button>
+                  <el-button style="margin-top: 12px;" type="danger" @click="submit">提交</el-button>
                 </div>
               </div>
 
@@ -119,44 +125,30 @@
 
 <script>
   import coo from '../cookie'
+
   export default {
     name: "Enterprise_examination",
     data() {
-      const generateData = _ => {
-        const data = [];
-        const cities = ['张三', '李四', '小青蛙'];
-        cities.forEach((city, index) => {
-          data.push({
-            label: city,
-            key: index,
-          });
-        });
-        return data;
-      };
       return {
-        userType:"",
-        userName:"",
-        data: generateData(),
-        value: [],
+        userType: "",
+        userName: "",
+        data: [
+        ],
+        chooseValue: [],
         filterMethod(query, item) {
           return item.label.indexOf(query) > -1;
         },
         active: 1,
-        form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        },
-        form2: {
-          type: '',
-          time: '',
-          number: '',
-          source: ''
+        examStatus: true,
+        examination: {
+          name: "",
+          beginTime: "",
+          status: "",
+          type: "",
+          continueTime: new Date(),
+          endTime: "",
+          questionNumber: "",
+          userName:""
         },
       }
     },
@@ -169,10 +161,60 @@
       },
       goback() {
         this.$router.push({name: 'Enterprise_examination', params: {userType: this.userType, userName: this.userName}})
-      }
+      },
+      submit() {
+        if (this.examStatus)
+          this.examination.status = "正在进行";
+        else
+          this.examination.status = "未启动";
+        this.addExamination();
+      },
+      addExaminee(re){
+        this.$axios.post(
+          'http://localhost:8081/Examination/addExaminee', re)
+          .then((res) => {
+          }).catch((err) => {
+          console.log(err)
+        })
+      },
+      addExamination() {
+        this.examination.userName = coo.getCookie("userName");
+        this.$axios.post(
+          'http://localhost:8081/Examination/addExamination', this.examination)
+          .then((res) => {
+            let re ={
+              object : this.chooseValue,
+              userName:this.userName,
+              targetID:res.data.id
+            };
+            this.addExaminee(re);
+            this.$router.push({name:"Enterprise_examination"});
+          }).catch((err) => {
+          console.log(err)
+        })
+      },
+      findExamUser() {
+        let re ={
+          userName:this.userName
+        };
+        this.$axios.post(
+          'http://localhost:8081/Examination/findExamUser', re)
+          .then((res) => {
+            let temps = res.data;
+            temps.forEach((temp) => {
+              this.data.push({
+                label:temp.userName,
+                key :temp.id,
+              })
+            })
+          }).catch((err) => {
+          console.log(err)
+        })
+      },
     },
     mounted() {
       this.userName = coo.getCookie("userName");
+      this.findExamUser();
     }
   }
 </script>
